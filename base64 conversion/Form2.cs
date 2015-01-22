2f336415fdc15a18361f840d7aeaf65938bc4bb4
Saruchi Arora <arora.saruchi@gmail.com>
2015-01-21 18:12:06 -0800

    committed form3 for calculating age (dealing with dateTime object)

--- base64 conversion/Form2.cs
+++ base64 conversion/Form2.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Globalization;
+using System.Globalization; //included for dateTime objects
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -19,49 +19,24 @@ namespace base64_conversion
         }
 
         private void button1_Click(object sender, EventArgs e)
-        {            
-            String date1 = textBox1.Text;
-            String date2 = textBox2.Text;
-            String date3 = textBox3.Text;
-
-            DateTime dt1 = Convert.ToDateTime (date1);
-            DateTime dt2 = Convert.ToDateTime(date2);
-            DateTime dt3 = Convert.ToDateTime(date3);
-
-            String format1="dd/MM/yy";
+        {
+            String format1 = "dd/MM/yy";
             String format2 = "dd/MMM/yyyy";
             String format3 = "MMM-dd-yyyy";
 
-            //String dt1_fromatStr = dt1.ToString(format1);
-            //if (DateTime.TryParseExact(date1,  out dt1)) 
-                 if (DateTime .TryParseExact (date1 ,format1 ,new CultureInfo("en-US"),DateTimeStyles.None ,out dt1 ))
-            
-            //if (textBox1.Text != "" && date1  == dt1_fromatStr)
-            //{
-                textBox1.Text = dt1.ToLongDateString();
-           // }
+            textBox1.Text = DateParser(textBox1.Text, format1);
+            textBox2.Text = DateParser(textBox2.Text, format2);
+            textBox3.Text = DateParser(textBox3.Text, format3);
+
+        }
+
+        public String DateParser(String date1, String format1)
+        {
+            DateTime dtArg = new DateTime();
+            if (DateTime.TryParseExact(date1, format1, new CultureInfo("en-US"), DateTimeStyles.None, out dtArg))
+                return dtArg.ToLongDateString();
             else
-                MessageBox.Show(" please enter the value in Date Format: dd/MM/yy in place of " + textBox1.Text);                  
-
-           
-            String dt2_fromatStr = dt2.ToString(format2);
-
-            if (textBox2.Text != "" && date2  == dt2_fromatStr)
-            {
-                textBox2.Text = dt2.ToLongDateString();
-            }
-            else
-                MessageBox.Show(" please enter the value in Date Format: dd/MMM/yyyy in place of " + textBox2.Text);
-
-            
-            String dt3_fromatStr = dt3.ToString(format3);
-
-            if (textBox3.Text != "" && date3  == dt3_fromatStr)
-            {
-                textBox3.Text = dt3.ToLongDateString();
-            }
-            else
-                MessageBox.Show(" please enter the value in Date Format: MMM-dd-yyyy in place of " +textBox3 .Text  );
+                return "Please enter the date value in specified format";
 
         }
     }
---
+++ base64 conversion/Form3.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Globalization;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace base64_conversion
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DateTime dob = new DateTime();
+            if (DateTime.TryParseExact(textBox1.Text, "MM-dd-yyyy", new CultureInfo("en-US"), DateTimeStyles.None, out dob))
+            {
+               textBox2 .Text = ageCalculator(dob)+"";
+            }
+        }
+
+        public int ageCalculator(DateTime  dob)
+        {
+            int  age;
+            DateTime now = DateTime.Today;             
+            age = dob .CompareTo( now) ;
+            return age;           
+        }
+
+    }
+}
